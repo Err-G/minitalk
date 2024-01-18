a02c5078ca548cd9b937d2b7cfc86735907368c7
Err-G <eriklustosa@gmail.com>
2024-01-18 20:30:45 +0000

    server started

--- minitalk.h
+++ minitalk.h
@@ -1,37 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ecarvalh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/18 20:26:50 by ecarvalh          #+#    #+#             */
-/*   Updated: 2024/01/18 20:29:45 by ecarvalh         ###   ########.fr       */
+/*   Created: 2024/01/18 20:26:11 by ecarvalh          #+#    #+#             */
+/*   Updated: 2024/01/18 20:27:33 by ecarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#ifndef MINITALK_H
+# define MINITALK_H
 
-void ft_putnbr(int num)
-{
-	unsigned int	n;
+#include <unistd.h>
 
-	n = num;
-	if (num < 0)
-	{
-		n = -num;
-		write(1, "-", 1);
-	}
-	if (n >= 10)
-		ft_putnbr(n % 10);
-	write(1, &"0123456789"[n / 10], 1);
-}
-
-int main(int ac, char **av)
-{
-	int	pid;
-
-	pid = getpid();
-	ft_putnbr(pid);
-	return (0);
-}
+#endif
